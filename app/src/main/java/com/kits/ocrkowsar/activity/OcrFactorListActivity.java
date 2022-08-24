9246936aa46be481a7ec61a3f13721f4cffd32cf
milad fallah <milad.mfz73@gmail.com>
2022-08-24 14:25:12 +0430

    fix bug 11.05.09

--- app/src/main/java/com/kits/ocrkowsar/activity/OcrFactorListActivity.java
+++ app/src/main/java/com/kits/ocrkowsar/activity/OcrFactorListActivity.java
@@ -80,12 +80,12 @@ public class OcrFactorListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr_factor_list);
 
-        dialog1 = new Dialog(this);
-        dialog1.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog1.setContentView(R.layout.rep_prog);
-        TextView repw = dialog1.findViewById(R.id.rep_prog_text);
-        repw.setText("در حال خواندن اطلاعات");
-        dialog1.show();
+//        dialog1 = new Dialog(this);
+//        dialog1.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        dialog1.setContentView(R.layout.rep_prog);
+//        TextView repw = dialog1.findViewById(R.id.rep_prog_text);
+//        repw.setText("در حال خواندن اطلاعات");
+//        dialog1.show();
 
 
 
